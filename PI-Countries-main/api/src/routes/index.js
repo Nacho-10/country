5d57de7f1bb34fb68e6c4afbbbb1c2b78b6c40f1
Nacho-10 <ignaciomusa26@gmail.com>
2023-03-28 17:44:47 -0300

    backend1

--- PI-Countries-main/api/src/routes/index.js
+++ PI-Countries-main/api/src/routes/index.js
@@ -1,4 +1,7 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Country = require('../models/Country');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -8,7 +11,41 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+let config = {
+    method : 'get',
+    maxBodyLength : Infinity,
+    url : 'https://restcountries.com/v3/all',
+    headers : {},
+};
+router.get('/guardar-paises', async (req,res) => {
+    axios
+        .request(config)
+        .then((response) => {
+            response.data.map(async (country) => {
+                if (country.capital) {
+                    const countryToSave = {
+                        id: country.cca3,
+                        name: country.name.common,
+                        flag: country.flags[1],
+                        continent: country.region,
+                        capital: country.capital[0],
+                        subregion: country.subregion,
+                        area: country.area,
+                        population: country.population,
+                    };
+                    console.log(countryToSave);
+                    await Country.create({
+                        ...countryToSave,    //aca hay una línea que quiero reveer
+                    });
+                };
+            });
+            res.send('data');
+        })
+        .catch ((error) => {
+            console.log(error)
+        });
 
-
+        
+});
 
 module.exports = router;
