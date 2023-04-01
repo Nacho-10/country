9641858bd42fb0337a04390de73bdb197453a51b
Nacho-10 <ignaciomusa26@gmail.com>
2023-04-01 17:54:53 -0300

    backend

--- PI-Countries-main/api/src/routes/index.js
+++ PI-Countries-main/api/src/routes/index.js
@@ -1,51 +1,50 @@
 const { Router } = require('express');
 const axios = require('axios');
-const Country = require('../models/Country');
-
+const { Country } = require('../db.js');
+const countriesRouter = require('./countries.routes.js');
+const activitiesRouter = require('./activities.routes.js');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
+
+router.get('/guardar-paises', async (req, res) => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://restcountries.com/v3/all',
+    headers: {},
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      response.data.forEach(async (country) => {
+        if (country.capital) {
+          await Country.create({
+            id: country.cca3,
+            name: country.name.common,
+            flag: country.flags[1],
+            continent: country.region,
+            capital: country.capital[0],
+            subregion: country.subregion,
+            area: country.area,
+            population: country.population,
+          });
+        }
+      });
+
+      res.send('Se cargo la base de datos');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
-let config = {
-    method : 'get',
-    maxBodyLength : Infinity,
-    url : 'https://restcountries.com/v3/all',
-    headers : {},
-};
-router.get('/guardar-paises', async (req,res) => {
-    axios
-        .request(config)
-        .then((response) => {
-            response.data.map(async (country) => {
-                if (country.capital) {
-                    const countryToSave = {
-                        id: country.cca3,
-                        name: country.name.common,
-                        flag: country.flags[1],
-                        continent: country.region,
-                        capital: country.capital[0],
-                        subregion: country.subregion,
-                        area: country.area,
-                        population: country.population,
-                    };
-                    console.log(countryToSave);
-                    await Country.create({
-                        ...countryToSave,    //aca hay una línea que quiero reveer
-                    });
-                };
-            });
-            res.send('data');
-        })
-        .catch ((error) => {
-            console.log(error)
-        });
-
-        
-});
+router.use('/countries', countriesRouter);
+router.use('/activities', activitiesRouter);
 
 module.exports = router;
+
